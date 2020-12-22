5266816fd45531139594c431604faeaed6e39e76
Arjan <arakke@ziggo.nl>
2020-12-22 14:15:56 +0100

    Small change to sql query

--- src/main/java/com/bep/lingo/game/data/WordRepository.java
+++ src/main/java/com/bep/lingo/game/data/WordRepository.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Repository;
 //https://www.callicoder.com/spring-boot-jpa-hibernate-postgresql-restful-crud-api-example/
 @Repository
 public interface WordRepository extends JpaRepository<Word, Long> {
-    @Query(value = "SELECT word FROM lingo.words WHERE LENGTH(word) = 5 ORDER BY random() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT word FROM words WHERE LENGTH(word) = 5 ORDER BY random() LIMIT 1", nativeQuery = true)
     Word getFiveLetterWord();
-    @Query(value = "SELECT word FROM lingo.words WHERE LENGTH(word) = 6 ORDER BY random() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT word FROM words WHERE LENGTH(word) = 6 ORDER BY random() LIMIT 1", nativeQuery = true)
     Word getSixLetterWord();
-    @Query(value = "SELECT word FROM lingo.words WHERE LENGTH(word) = 7 ORDER BY random() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT word FROM words WHERE LENGTH(word) = 7 ORDER BY random() LIMIT 1", nativeQuery = true)
     Word getSevenLetterWord();
 }
